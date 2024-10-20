ae502ccd970b1640b5243cf352261a40c44bf3c6
Mih4n <losevmisha0@gmail.com>
2024-10-20 19:03:23 +0300

    fix profiles

--- src/endstone_whitelist/listener.py
+++ src/endstone_whitelist/listener.py
@@ -7,11 +7,12 @@ from endstone_whitelist.tools.config_provider import GetConfiguration
 class Listener:
     def __init__(self, plugin: Plugin):
         self._plugin = plugin
+        self._config = GetConfiguration("config")
 
     @event_handler
     def on_player_join(self, event: PlayerLoginEvent):
         player = event.player
         event.kick_message = "You are not whitelisted"
-        whitelist = GetConfiguration("whitelist")
+        whitelist = GetConfiguration(self._config["profile"])
         if player.name not in whitelist:
             event.cancelled = True
