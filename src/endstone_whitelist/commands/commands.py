2cd2f79c256c2d3f1dc12d54d3416e8ea2a262a8
mih4n <losevmisha0@gmail.com>
2026-04-17 17:34:46 +0300

    fix

--- src/endstone_whitelist/commands/commands.py
+++ src/endstone_whitelist/commands/commands.py
@@ -3,5 +3,5 @@ from typing import TYPE_CHECKING
 from .whitelist import WhitelistCommandExecutor 
 if TYPE_CHECKING: from endstone_whitelist.plugin import WhitelistPlugin 
 
-def register_commands(plugin: WhitelistPlugin):
+def register_commands(plugin: "WhitelistPlugin"):
     plugin.get_command("wl").executor = WhitelistCommandExecutor(plugin)
